e9e6179b23ac8397650cd6dbea76c80b7631fec0
Jeremy Guillette <jguillette@fas.harvard.edu>
2021-03-16 13:31:57 -0400

    added update script

---
+++ update.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import yaml
+import os
+import shutil
+import requests
+import zipfile
+
+
+# In[2]:
+
+
+with open("plugins.yml", 'r') as fp:
+    plugins = yaml.load(fp, Loader=yaml.FullLoader)
+
+
+# In[3]:
+
+
+# nuke everything with a few exceptions
+doNotRemove = [
+    '.git',
+    '.gitignore',
+    'plugins.yml',
+    'update.py'
+]
+
+
+# In[4]:
+
+
+for thing in os.listdir('.'):
+    if thing not in doNotRemove and os.path.isdir(thing):
+        shutil.rmtree(thing)
+print('Old plugins removed')
+
+
+# In[5]:
+
+
+def update_plugin(pluginName, pluginInfo):
+    print(f'Downloading {pluginName}...')
+    filename = pluginInfo['name'] + '.zip'
+    R = requests.get(pluginInfo['url'])
+    with open(filename, 'wb') as fp:
+        fp.write(R.content)
+
+    print(f'Unzipping {pluginName}...')
+    with zipfile.ZipFile(filename,"r") as zip_ref:
+        roots = []
+        for name in zip_ref.namelist():
+            pathList = name.split('/')
+            if pathList[0] not in roots:
+                roots.append(pathList[0])
+        if len(roots) == 1:
+            root_dir = roots[0]
+        else:
+            print(f'{pluginName} has multiple root directories/files, which is not the expected directory structure. This script probably needs to be updated.')
+            return False
+        zip_ref.extractall('.')
+        os.rename(root_dir, pluginInfo['name'])
+
+    print(f'Removing zip file for {pluginName}...')
+    os.remove(filename)
+
+    print(f'Update of {pluginName} complete!')
+    
+    return True
+
+
+# In[6]:
+
+
+for pluginName, pluginInfo in plugins.items():
+    update_plugin(pluginName, pluginInfo)
+
