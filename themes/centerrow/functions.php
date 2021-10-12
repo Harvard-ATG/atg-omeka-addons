e8b2469741d1e1971da06be446b90eb7c6ad3c0b
Cole Crawford <cole_crawford@harvard.edu>
2021-10-12 19:30:38 -0400

    Update themes
    - Skipping "The Daily" - zip name changed, wasn't sure about it. Everything else is a minor version update. Need to get Eileen Southern changes in.

--- themes/centerrow/functions.php
+++ themes/centerrow/functions.php
@@ -35,4 +35,24 @@ function centerrow_output_text_track_file($textFile) {
 
     return $track;
 }
+
+function centerrow_check_for_tracks($files) {
+    foreach ($files as $file) {
+        if ($file->getExtension() == "vtt") {
+            return true;
+        }
+    }
+    return false;
+}
+
+function centerrow_get_square_thumbnail_url($file, $view) {
+    if ($file->hasThumbnail()) {
+        $squareThumbnail = file_display_url($file, 'square_thumbnail');
+    } else {
+        $mimeType = $file->mime_type;
+        $fileType = (strpos($mimeType, 'image')) ? 'image' : 'video';
+        $squareThumbnail = $view->baseUrl() . '/application/views/scripts/images/fallback-' . $fileType . '.png';
+    }
+    return $squareThumbnail;
+}
 ?>
